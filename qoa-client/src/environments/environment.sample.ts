e381ca29913b816231eb6c9bf610c5b258fedfd6
Sergiu <szatmari.sergiu@gmail.com>
2021-02-17 09:41:00 +0200

    Min

--- qoa-client/src/environments/environment.sample.ts
+++ qoa-client/src/environments/environment.sample.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080/api',
-  socketEndpoint: 'http://localhost:8079',
+  socketEndpoint: 'ws://localhost:8079',
   auth0: {
     domain: '',
     clientId: '',
